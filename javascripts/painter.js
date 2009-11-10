b214746a8e2ea3315a1ae1007d311be8d9478237
DylanFM <dylan.fm@gmail.com>
2009-11-10 17:55:35 +0900

    We've got some simple painting going on.

--- javascripts/painter.js
+++ javascripts/painter.js
@@ -1,22 +1,35 @@
 $(function() {
   
-  var painting = Raphael("painting", 900, 600);
+  var painting = Raphael("painting", 900, 600),
+      active = false;
   
-  var get_click_coordinates = function(event) {
-    // Return X and Y coordinates for this click event
-    return [
-      (event.pageX - event.srcElement.offsetLeft),
-      (event.pageY - event.srcElement.offsetTop)
-    ];
+  var get_event_coordinates = function(event) {
+    // Return X and Y coordinates for this event
+    return [(event.pageX - event.srcElement.offsetLeft),
+            (event.pageY - event.srcElement.offsetTop)];
   };
   
-  // If there's a click in the painting
-  $(painting.node).bind('click', function(e) {
-    var coords = get_click_coordinates(e),
-        x;
+  var circle_at = function(coords) {
     // Add a dot where they clicked
-    x = painting.circle(coords[0], coords[1], 2);
+    var x = painting.circle(coords[0], coords[1], 2);
     x.attr({ fill: "black" });
+  };
+  
+  var paint = function(coords) {
+    circle_at(coords);
+  };
+  
+  // Test code for mouse down
+  $(painting.node).bind("mousedown", function(e) {
+    active = true;
+  });
+  $(painting.node).bind("mouseup", function(e) {
+    active = false;
+  });
+  $(painting.node).bind("mousemove", function(e) {
+    if (active === true) {
+      paint(get_event_coordinates(e));
+    }
   });
   
 });
