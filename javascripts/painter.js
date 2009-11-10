fb3615bfd2e5ab602afbe7ca6d510804e4dc7ba6
DylanFM <dylan.fm@gmail.com>
2009-11-10 20:17:24 +0900

    Just some commenting.

--- javascripts/painter.js
+++ javascripts/painter.js
@@ -4,6 +4,7 @@ $(function() {
       active = false,
       dots = [];
   
+  // Get coords
   var get_event_coordinates = function(e) {
     var src = (e.srcElement || e.originalTarget.parentNode);
     // Return X and Y coordinates for this event
@@ -11,18 +12,22 @@ $(function() {
             (e.pageY - src.offsetTop)];
   };
   
+  // Add a dot
   var paint = function(coords) {
-    // Add a dot where they clicked
     return painting.circle(coords[0], coords[1], 3).attr({ fill: "black" });
   };
   
-  // Test code for mouse down
+  // When the mouse goes down we paint
   $(painting.node).bind("mousedown", function(e) {
     active = true;
   });
+  
+  // When the mouse goes up we don't paint
   $(painting.node).bind("mouseup", function(e) {
     active = false;
   });
+  
+  // When the mouse moves we might paint... it depends
   $(painting.node).bind("mousemove", function(e) {
     var dot;
     if (active === true) {
@@ -32,6 +37,7 @@ $(function() {
     }
   });
   
+  // So one can remove their rubbish
   $("a#clear").bind("click", function(e) {
     $.each(dots, function(i) {
       this.point.remove();
